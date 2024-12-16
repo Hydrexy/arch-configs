7815f49c90b3b2b163bb45d1c2b04eb1410b3910
hydrexy <dantederenzo@icloud.com>
2024-12-15 19:58:23 -0500

    fixed script

--- set-up.sh
+++ set-up.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh
 
-suod cp -ru etc/* /etc
+dir=$(dirname "$(realpath"$0")")
 
-sudo cp -ru root/* /root
+suod cp -ru "$dir/etc/*" /etc
 
-sudo cp -ru skel/* /etc/skel
+sudo cp -ru "$dir/root/*" /root
 
-sudo cp -ru usr/share/konsole/*
+sudo cp -ru "$dir/skel/*" /etc/skel
 
-sudo cp -ru usr/share/vim/vim91/*
+sudo cp -ru "$dir/usr/share/konsole/*" /usr/share/konsole/
+
+sudo cp -ru "$dir/usr/share/vim/vim91/*" /usr/share/vim/vim91/
